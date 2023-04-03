d7f1e1f7927874f24b38f801b6ea8f28335d4455
Rodrigo Quiroga <rodrigo.menduco19@gmail.com>
2023-04-03 10:11:41 -0300

    Arreglos menores y se agrego un archivo

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
-function openDetails() {
-    document.getElementById("myDetails").open = true;
-  }
-  
   function closeDetails() {
-    document.getElementById("myDetails").open = false;
+    document.getElementById("myDetails").open = false;    
+  }
+
+  function closeSummary(){
+    document.getElementById("mySummary").open = false;
+  }
+
+  function closeSummary1(){
+    document.getElementById("mySummary1").open = false;
   }
