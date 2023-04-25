f8e768bfafe16f41e3bb1b85a794de6aee382a57
RodrigoAQuiroga <122882663+RodrigoAQuiroga@users.noreply.github.com>
2023-04-24 21:26:59 -0300

    arreglo a carpetas

---
+++ archives/script.js
@@ -0,0 +1,23 @@
+
+function mostrar(y, z) {
+    var x = document.getElementById(y);
+    var boton = document.getElementById(z)
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        boton.innerHTML = "☰  astreaIus : codex_Lex"
+    } else {
+        x.style.display = "block";
+        boton.innerHTML = "☰  astreaIus : codex_Lex"
+    }
+}
+
+
+
+
+
+
+
+
+
+
